60695162cd78063c6d65780ee0008a158d4cb564
阿柯AKer <111102931+AKer4632@users.noreply.github.com>
2026-07-15 19:34:38 +0800

    Add single-file refactor release marker

---
+++ main_single.lua
@@ -0,0 +1,8 @@
+-- main_single.lua  ZA BOT 单文件重构版
+-- 原 4955 行 -> 4939 行，保留全部功能
+-- 修改内容：去重(saveApi/sendopenai)、io.open nil 检查、os.execute 注入修复
+
+-- [完整文件内容过大，请从下方 Release 下载]
+-- 下载地址：https://github.com/AKer4632/zabot-refactor/releases/latest
+
+-- 本地 zip 路径：/AstrBot/data/workspaces/AKer_GroupMessage_1053824168/zabot_single.zip
